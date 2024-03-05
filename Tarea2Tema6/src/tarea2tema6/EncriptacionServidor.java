c7c4f9686a33d30d4140e57678280137019ca0db
rnunher1411 <145585915+rnunher1411@users.noreply.github.com>
2024-03-05 20:26:42 +0100

    Empezando la tarea 2

---
+++ Tarea2Tema6/src/tarea2tema6/EncriptacionServidor.java
@@ -0,0 +1,16 @@
+package tarea2tema6;
+
+/**
+ *
+ * @author rnunher1411
+ */
+public class EncriptacionServidor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Tarea2Tema6/src/tarea2tema6/EncriptaciónCliente.java
@@ -0,0 +1,9 @@
+package tarea2tema6;
+
+/**
+ *
+ * @author rnunher1411
+ */
+public class EncriptaciónCliente {
+    
+}
